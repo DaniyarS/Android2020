6c52327ffa0b6db7499cc70e049b2057b436e8bf
daniyars <dslamkul14@gmail.com>
2020-02-28 22:19:46 +0600

    dd

--- VkFragment/app/src/main/java/com/example/vkfragment/HomeFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.vkfragment;
-
-import androidx.fragment.app.Fragment;
-
-public class HomeFragment extends Fragment {
-
-}
--- VkFragment/app/src/main/java/com/example/vkfragment/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.vkfragment;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onItemClickListener(View v, int position);
-}
