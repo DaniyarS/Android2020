8a088f1f3c3c68316afd8e4f474819efbf931d10
daniyars <dslamkul14@gmail.com>
2020-03-13 23:58:27 +0600

    init commit

--- VkFragment/app/src/main/java/com/example/vkontakte/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.vkontakte;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onItemClickListener(View v, int position);
-}
--- Vkontakte/app/src/main/java/com/example/vkontakte/ListItem.java
+++ Vkontakte/app/src/main/java/com/example/vkontakte/ListItem.java
@@ -3,60 +3,60 @@ package com.example.vkontakte;
 
 public class ListItem {
     private String groupName;
-    private String publishDate;
-    private String heading;
-    private String likes;
-    private String comments;
-    private String views;
-    private String imgURL;
-    private String shares;
+        private String publishDate;
+        private String heading;
+        private String likes;
+        private String comments;
+        private String views;
+        private String imgURL;
+        private String shares;
 
-    public String getShares() {
-        return shares;
-    }
+        public String getShares() {
+            return shares;
+        }
 
-    public void setShares(String shares) {
-        this.shares = shares;
-    }
+        public void setShares(String shares) {
+            this.shares = shares;
+        }
 
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
+        public void setGroupName(String groupName) {
+            this.groupName = groupName;
+        }
 
-    public void setPublishDate(String publishDate) {
-        this.publishDate = publishDate;
-    }
+        public void setPublishDate(String publishDate) {
+            this.publishDate = publishDate;
+        }
 
-    public void setHeading(String heading) {
-        this.heading = heading;
-    }
+        public void setHeading(String heading) {
+            this.heading = heading;
+        }
 
-    public void setLikes(String likes) {
-        this.likes = likes;
-    }
+        public void setLikes(String likes) {
+            this.likes = likes;
+        }
 
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
+        public void setComments(String comments) {
+            this.comments = comments;
+        }
 
-    public void setViews(String views) {
-        this.views = views;
-    }
+        public void setViews(String views) {
+            this.views = views;
+        }
 
-    public void setImgURL(String imgURL) {
-        this.imgURL = imgURL;
-    }
+        public void setImgURL(String imgURL) {
+            this.imgURL = imgURL;
+        }
 
-    public String getGroupName() {
-        return groupName;
-    }
+        public String getGroupName() {
+            return groupName;
+        }
 
-    public String getPublishDate() {
-        return publishDate;
-    }
+        public String getPublishDate() {
+            return publishDate;
+        }
 
-    public String getHeading() {
-        return heading;
+        public String getHeading() {
+            return heading;
     }
 
     public String getLikes() {
